9b0d4c918ebc07fcc320a3807edcc4d990fc4b4e
OhYongUn <dhdyddjs@naver.com>
2024-07-28 16:10:19 +0900

    test workspace삭제

--- my-turborepo/apps/user/components/utils.ts
+++
@@ -1,8 +0,0 @@
-import type {ClassValue} from "clsx";
-import {twMerge} from "tailwind-merge";
-import {clsx} from "clsx";
-
-export function cn(...inputs: ClassValue[]) {
-    console.log('cscscs')
-    return twMerge(clsx(inputs))
-}
