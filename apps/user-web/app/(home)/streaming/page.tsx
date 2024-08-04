6b9915c4c570c38460ff67c66c8f83bc95f6bea7
OhYongUn <dhdyddjs@naver.com>
2024-08-04 17:52:22 +0900

    url 별 헤더 푸터 설정

--- apps/user-web/app/(home)/streaming/page.tsx
+++ apps/user-web/app/(home)/streaming/page.tsx
@@ -1,13 +1,16 @@
-import LiveStreamVideo from "../../../components/liveStreamVideo";
-import LiveChatBox from "../../../components/liveChatBox";
+import LiveStreamVideo from '../../../components/liveStreamVideo';
+import LiveChatBox from '../../../components/liveChatBox';
 
 const StreamingPage = () => {
-    return (
-            <div className="flex-1 flex flex-col md:flex-row">
-                <LiveStreamVideo/>
-                <LiveChatBox/>
-            </div>
-    );
-
-}
+  return (
+    <div className="flex flex-col h-screen">
+      <main className="flex flex-col items-center p-4 md:flex-row md:justify-between md:p-8">
+        <div className="flex-1 flex flex-col md:flex-row">
+          <LiveStreamVideo />
+          <LiveChatBox />
+        </div>
+      </main>
+    </div>
+  );
+};
 export default StreamingPage;
--- apps/user-web/components/ConditionalHeaderFooter.tsx
+++ apps/user-web/components/ConditionalHeaderFooter.tsx
@@ -1,17 +1,22 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import Header from "@app/components/layout/Header";
-import Footer from "@app/components/layout/Footer";
+import Header from '@app/components/layout/Header';
+import Footer from '@app/components/layout/Footer';
+import StreamingHeader from '@app/components/streamingHeader';
 
 const pagesWithoutHeaderFooter = ['/streaming'];
 
-export default function ConditionalHeaderFooter({ children }: { children: React.ReactNode }) {
+export default function ConditionalHeaderFooter({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const pathname = usePathname();
   const showHeaderFooter = !pagesWithoutHeaderFooter.includes(pathname);
-
   return (
     <>
+      {pathname === '/streaming' && <StreamingHeader />}
       {showHeaderFooter && <Header />}
       {children}
       {showHeaderFooter && <Footer />}
